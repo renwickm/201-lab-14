f5b8910ba35076bf4052b9255d9829ec937a8d26
Liliane Lendvai <lilianelendvai@gmail.com>
2022-07-28 16:31:59 -0700

    added lab 14 JavaScript code

--- js/app.js
+++ js/app.js
@@ -7,11 +7,14 @@ const Cart = function(items) {
 };
 
 Cart.prototype.addItem = function(product, quantity) {
-  // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+  // DONE: Fill in this instance method to create a new CartItem and add it to this.items
+  let cartItem = new CartItem(product, quantity);
+  this.items.push(cartItem);
 };
 
 Cart.prototype.saveToLocalStorage = function() {
-  // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  // DONE: Fill in this instance method to save the contents of the cart to localStorage
+  localStorage.setItem('myCartItems',JSON.stringify(this.items));
 };
 
 Cart.prototype.removeItem = function(item) {
