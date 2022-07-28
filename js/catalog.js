d8a0bd06e445292c31d1322b85c754bdff08cd01
Matthew T. Renwick <mrenny65@gmail.com>
2022-07-28 14:41:59 -0700

    added option tag to HTML via JS manipulation

--- js/catalog.js
+++ js/catalog.js
@@ -12,7 +12,10 @@ function populateForm() {
   //TODO: Add an <option> tag inside the form's select for each product
   const selectElement = document.getElementById('items');
   for (let i in Product.allProducts) {
-
+    let option = document.createElement('option');
+    option.textContent = Product.allProducts[i].name;
+    option.value = Product.allProducts[i].name;
+    selectElement.appendChild(option);
   }
 
 }
@@ -24,7 +27,10 @@ function handleSubmit(event) {
 
   // TODO: Prevent the page from reloading
 
+  event.preventDefault();
+
   // Do all the things ...
+
   addSelectedItemToCart();
   cart.saveToLocalStorage();
   updateCounter();
@@ -33,7 +39,9 @@ function handleSubmit(event) {
 }
 
 // TODO: Add the selected item and quantity to the cart
+
 function addSelectedItemToCart() {
+  
   // TODO: suss out the item picked from the select list
   // TODO: get the quantity
   // TODO: using those, add one item to the Cart
